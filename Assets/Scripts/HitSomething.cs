78aee1aa78564c91189455a210a5f9dfc1752761
WalterSchirmacher <wschirmahcer63@gmail.com>
2023-02-19 13:07:30 -0500

    Hit Trees, Rocks and Bushes working. Good FPS.

--- Assets/Scripts/HitSomething.cs
+++ Assets/Scripts/HitSomething.cs
@@ -6,36 +6,71 @@ using UnityEngine.Audio;
 public class HitSomething : MonoBehaviour
 {
 
-
-    public AudioSource audioSourceRock, audioSourceTree;
+    public enum ManOrWoman { Man, Woman };
+    public ManOrWoman manOrWoman = ManOrWoman.Man;
+    public AudioSource audioSourceRock, audioSourceTree, audioSourceBush;
+    public AudioClip _rockSoundMan, _rockSoundWoman, _treeSoundMan, _treeSoundWoman, _bushSoundMan, _bushSoundWoman;
 
     public int rockHits = 0;
     public int treeHits = 0;
+    public int bushHits = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-       // audioSourceRock.Play();
+        audioSourceRock.GetComponent<AudioSource>();
+        audioSourceTree.GetComponent<AudioSource>();
+
+        if (audioSourceRock && audioSourceTree && audioSourceBush && _rockSoundMan && _treeSoundMan && _bushSoundMan && _rockSoundWoman && _treeSoundWoman && _bushSoundWoman)
+        {
+            Debug.Log("Audio sources good");
+
+            if(manOrWoman == ManOrWoman.Man)
+            {
+                audioSourceRock.clip = _rockSoundMan;
+                audioSourceTree.clip = _treeSoundMan;
+                audioSourceBush.clip = _bushSoundMan;
+            } else
+            {
+                audioSourceRock.clip = _rockSoundWoman;
+                audioSourceTree.clip = _treeSoundWoman;
+                audioSourceBush.clip = _bushSoundWoman;
+            }
+        } else
+        {
+            Debug.Log("Invalid Audio Source or Clip!");
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
-        {
+    {
         string theTag = collision.gameObject.tag;
-            if (theTag == "Rock")
-            {
+
+        switch (theTag)
+        {
+            case "Rock":
                 rockHits++;
                 audioSourceRock.Play();
-            Debug.Log("Hit a rock");
-            } else if (theTag == "Tree")
-            {
+                break;
+            case "Tree":
                 treeHits++;
-               audioSourceTree.Play();
-            Debug.Log("Hit a tree!");
-             } else
-            {
-                Debug.Log("Collided with something other than a Rock or a Tree");
-            }
-       // collision.gameObject.SetActive(false);
+                audioSourceTree.Play();
+                break;
+            case "Bush":
+                bushHits++;
+                audioSourceBush.Play();
+                break;
+            default:
+                Debug.Log("Collided with something other than a Rock, Tree, or a Bush");
+                break;
         }
+
+        if(theTag != string.Empty)
+        {
+            Debug.Log("Hit a " + theTag);
+        }
+       
+        // collision.gameObject.SetActive(false);
+    }
 }
