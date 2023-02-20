b44f53524f3205be293fb3bc716b63a5cd9a48a8
WalterSchirmacher <wschirmahcer63@gmail.com>
2023-02-20 17:31:57 -0500

    Friends and Foes Updated

--- Assets/Scripts/FriendOrFoe.cs
+++ Assets/Scripts/FriendOrFoe.cs
@@ -11,22 +11,36 @@ public class FriendOrFoe : MonoBehaviour
     public bool moveParent = false;
     public GameObject parentObject;
     public bool makeTransparent = false;
+    public bool changeTransparency = true;
     public float minAlpha = 0;
-    public float maxAlpha = 0;
-    public float currentAlpha;
+    public float maxAlpha = 30;
+
+    private float currentAlpha;
+    private bool changedToTrans = false;
 
     // Start is called before the first frame update
     void Start()
     {
         if(makeTransparent)
         {
-            Renderer ren = GetComponent<Renderer>();
-            float matMode = ren.material.GetFloat("_mode");
+            MakeTransparent();
+        }
 
-            if (matMode == 0 || matMode == 1)
+    }
+
+    public void MakeTransparent()
+    {
+        Debug.Log("Make Transparent");
+
+        if(changeTransparency)
+        {
+            Renderer ren = GetComponent<Renderer>();
+
+            if(!changedToTrans)
             {
-                StandardShaderUtils.ChangeRenderMode(ren.material, StandardShaderUtils.BlendMode.Transparent);
+                ChangeMaterialToTrans(ren);
             }
+
             currentAlpha = ren.material.color.a;
 
             if (currentAlpha != minAlpha)
@@ -39,5 +53,43 @@ public class FriendOrFoe : MonoBehaviour
         }
 
     }
+
+    public void MakeSolid()
+    {
+        Debug.Log("Make Solid");
+
+        if (changeTransparency)
+        {
+            Renderer ren = GetComponent<Renderer>();
+
+            if (!changedToTrans)
+            {
+                ChangeMaterialToTrans(ren);
+            }
+
+            currentAlpha = ren.material.color.a;
+
+            if (currentAlpha != maxAlpha)
+            {
+                Color color = ren.material.color;
+                color.a = maxAlpha;
+                ren.material.color = color;
+                currentAlpha = maxAlpha;
+            }
+        }
+         
+    }
+
+    void ChangeMaterialToTrans(Renderer theRen)
+    {
+        float matMode = theRen.material.GetFloat("_mode");
+
+        if (matMode == 0 || matMode == 1)
+        {
+            StandardShaderUtils.ChangeRenderMode(theRen.material, StandardShaderUtils.BlendMode.Transparent);
+        }
+        changedToTrans = true;
+    }
+    
 
 }
--- Assets/Scripts/Sight.cs
+++ Assets/Scripts/Sight.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Sight : MonoBehaviour
@@ -8,7 +9,10 @@ public class Sight : MonoBehaviour
     public float angle;
     public LayerMask objectsLayers;
     public LayerMask obstancesLayers;
-    public Collider detectedObjects;
+  //  public List<Collider> detectedObjects = new List<Collider>();
+
+    [SerializeField] private List<FriendOrFoe> currentlyVisible;
+    [SerializeField] private List<FriendOrFoe> alreadyTransparent;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +23,55 @@ public class Sight : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        GetAllVisibleObjects();
+
+        MakeObjectsSolid();
+        MakeObjectsTransparent();
+
+        /*  Collider[] colliders = Physics.OverlapSphere(transform.position, distance, (int)objectsLayers);
+
+          detectedObjects.Clear();
+
+          for (int i = 0; i < colliders.Length; i++)
+          {
+              Collider collider = colliders[i];
+
+              Vector3 directionToController = Vector3.Normalize(collider.bounds.center - transform.position);
+
+              float angleToCollider = Vector3.Angle(transform.forward, directionToController);
+
+              if (angleToCollider < angle)
+              {
+                  if(!Physics.Linecast(transform.position, collider.bounds.center, (int)obstancesLayers))
+                  {
+                      detectedObjects.Add(collider);
+                      break;
+                  }
+              }
+          }
+          if(detectedObjects.Any())
+          {
+              Debug.Log("I see the following:");
+              detectedObjects.ForEach(delegate (string name))
+              {
+                  Debug.Log(name);
+              }
+
+          } else
+          {
+              Debug.Log("Nothing in sight!");
+          } */
+
+    }
+
+    private void GetAllVisibleObjects()
+    {
+        currentlyVisible.Clear();
+
         Collider[] colliders = Physics.OverlapSphere(transform.position, distance, (int)objectsLayers);
 
-        detectedObjects = null;
+        currentlyVisible.Clear();
 
         for (int i = 0; i < colliders.Length; i++)
         {
@@ -33,27 +83,68 @@ public class Sight : MonoBehaviour
 
             if (angleToCollider < angle)
             {
-                if(!Physics.Linecast(transform.position, collider.bounds.center, (int)obstancesLayers))
+                if (!Physics.Linecast(transform.position, collider.bounds.center, (int)obstancesLayers))
                 {
-                    detectedObjects = collider;
+                  //  detectedObjects.Add(collider);
+
+                    if (collider.gameObject.TryGetComponent(out FriendOrFoe friendOrFoe))
+                    {
+                        if (!currentlyVisible.Contains(friendOrFoe))
+                        {
+                            currentlyVisible.Add(friendOrFoe);
+                            Debug.Log("I see the following:" + friendOrFoe.name);
+                        }
+                    }
+
                     break;
                 }
             }
         }
-        if(detectedObjects)
+       /* if (detectedObjects.Any())
         {
             Debug.Log("I see the following:");
-            Debug.Log(detectedObjects.name);
-        } else
+            detectedObjects.ForEach(delegate (string name))
+            {
+                Debug.Log(name);
+            }
+
+        }
+        else
         {
             Debug.Log("Nothing in sight!");
+        } */
+    }
+
+    private void MakeObjectsTransparent()
+    {
+        for (int i = 0; i < currentlyVisible.Count; i++)
+        {
+            FriendOrFoe friendOrFoe = currentlyVisible[i];
+            if (!alreadyTransparent.Contains(friendOrFoe))
+            {
+                friendOrFoe.MakeTransparent();
+                alreadyTransparent.Add(friendOrFoe);
+            }
         }
     }
 
-    private void OnDrawGizmos()
+    private void MakeObjectsSolid()
     {
-        Gizmos.color = Color.red;
-     //Use the same vars you use to draw your Overlap SPhere to draw your Wire Sphere.
-     Gizmos.DrawWireSphere(transform.position, distance);
+        for (int i = alreadyTransparent.Count - 1; i >= 0; i--)
+        {
+            FriendOrFoe friendOrFoe = alreadyTransparent[i];
+            if (!currentlyVisible.Contains(friendOrFoe))
+            {
+                friendOrFoe.MakeSolid();
+                alreadyTransparent.Remove(friendOrFoe);
+            }
+        }
     }
+
+    /* private void //OnDrawGizmos()
+     {
+         Gizmos.color = Color.red;
+      //Use the same vars you use to draw your Overlap SPhere to draw your Wire Sphere.
+      Gizmos.DrawWireSphere(transform.position, distance);
+     } */
 }
