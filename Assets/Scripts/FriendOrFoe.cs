9f621eed6f9f7535d08bb3e852ecb58b56da587f
WalterSchirmacher <wschirmahcer63@gmail.com>
2023-02-20 21:52:25 -0500

    Adjusted Friend or For script for LOD

--- Assets/Scripts/FriendOrFoe.cs
+++ Assets/Scripts/FriendOrFoe.cs
@@ -6,90 +6,70 @@ public class FriendOrFoe : MonoBehaviour
 {
 
     public enum MyStatus { Friendly, Neutral, Hostile, ExtremeHatred };
+    public enum TransStatus { UseMin, UseMax, None };
     public MyStatus myStatus = MyStatus.Neutral;
     public GameObject mainObject;
-    public bool moveParent = false;
     public GameObject parentObject;
-    public bool makeTransparent = false;
+    public TransStatus transparancyState = TransStatus.None;
     public bool changeTransparency = true;
     public float minAlpha = 0;
     public float maxAlpha = 30;
 
     private float currentAlpha;
-    private bool changedToTrans = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        if(makeTransparent)
+        if(transparancyState == TransStatus.UseMin)
         {
-            MakeTransparent();
+            MakeTransparent();     
+        } else if (transparancyState == TransStatus.UseMax)
+        {
+            MakeSolid();
         }
-
     }
 
+    [ContextMenu("Make Transparent")]
     public void MakeTransparent()
     {
         Debug.Log("Make Transparent");
 
-        if(changeTransparency)
+        if (changeTransparency)
         {
-            Renderer ren = GetComponent<Renderer>();
-
-            if(!changedToTrans)
-            {
-                ChangeMaterialToTrans(ren);
-            }
-
-            currentAlpha = ren.material.color.a;
-
-            if (currentAlpha != minAlpha)
-            {
-                Color color = ren.material.color;
-                color.a = minAlpha;
-                ren.material.color = color;
-                currentAlpha = minAlpha;
-            }
+            ChangeTransparancy(mainObject, minAlpha);
         }
-
     }
 
+    [ContextMenu("Make Solid")]
     public void MakeSolid()
     {
         Debug.Log("Make Solid");
 
         if (changeTransparency)
         {
-            Renderer ren = GetComponent<Renderer>();
-
-            if (!changedToTrans)
-            {
-                ChangeMaterialToTrans(ren);
-            }
-
-            currentAlpha = ren.material.color.a;
-
-            if (currentAlpha != maxAlpha)
-            {
-                Color color = ren.material.color;
-                color.a = maxAlpha;
-                ren.material.color = color;
-                currentAlpha = maxAlpha;
-            }
+                ChangeTransparancy(mainObject, maxAlpha);
         }
          
     }
 
-    void ChangeMaterialToTrans(Renderer theRen)
-    {
-        float matMode = theRen.material.GetFloat("_mode");
 
-        if (matMode == 0 || matMode == 1)
+    public void ChangeTransparancy(GameObject theObj, float theAlpha)
+    {
+        Renderer ren = theObj.GetComponent<Renderer>();
+
+        currentAlpha = ren.material.color.a;
+
+        if (currentAlpha != theAlpha)
         {
-            StandardShaderUtils.ChangeRenderMode(theRen.material, StandardShaderUtils.BlendMode.Transparent);
+            Color color = ren.material.color;
+            color.a = minAlpha;
+            ren.material.color = color;
+            currentAlpha = theAlpha;
         }
-        changedToTrans = true;
+
     }
+
     
 
 }
